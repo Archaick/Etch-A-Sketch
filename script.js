ae8a1bd8bb3ee9b83636307292c98957cb59182d
Archaick <saeed.jh3@gmail.com>
2023-06-05 18:47:46 +0700

    JS is functional, basic styling added too

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 const grid = document.getElementById('container');
 let gridIndex = 0;
 
-while (gridIndex < (20*20)) {
+while (gridIndex < (50*50)) {
     let gridLayer = document.createElement('div');
     gridLayer.classList.add('minibox');
     grid.appendChild(gridLayer);
     gridIndex++;
 }
 
-// find all elements with .minibox class and store in elements variable
+
+// Find all elements with .minibox class and store in elements variable
 const elements = document.querySelectorAll('.minibox');
 
-// Add event listeners for the 'mouseenter' and 'mouseleave' events to each element
+// Variable to track the state of the "W" key
+let isWKeyDown = false;
+
+// Add event listener for the 'mouseenter' event to each element
 elements.forEach(element => {
-    element.addEventListener('mouseenter', handleMouseEnter);
+  element.addEventListener('mouseenter', handleMouseEnter);
 });
 
-hoverEffect = function (element) {
-    element.addEventListener('mouseenter', handleMouseEnter);
-}
-elements.forEach(hoverEffect);
+// Add event listeners for the 'keydown' and 'keyup' events on the document
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
 
 // Event handler for mouse enter
 function handleMouseEnter(event) {
-  // Add a CSS class to the element when the mouse enters
-  event.target.classList.add('filled');
+  // Check if the "W" key is currently held down
+  if (isWKeyDown) {
+    event.target.classList.add('filled');
+  }
 }
 
+// Event handler for keydown event
+function handleKeyDown(event) {
+  // Check if the "W" key is pressed
+  if (event.key === 'w' || event.key === 'W') {
+    isWKeyDown = true;
+  }
 
-// Reset button
+  // Check if the "S" key is pressed
+  if (event.key === 's' || event.key === 'S') {
+    const elementsToReset = document.querySelectorAll('.filled');
+    elementsToReset.forEach(element => {
+      element.classList.remove('filled');
+    });
+  }
+}
+
+// Event handler for keyup event
+function handleKeyUp(event) {
+  // Check if the "W" key is released
+  if (event.key === 'w' || event.key === 'W') {
+    isWKeyDown = false;
+  }
+}
+
+/* Erase button */
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', handleReset);
-
 function handleReset() {
   // Reset the class on your desired elements
   const elementsToReset = document.querySelectorAll('.filled');
